208306510d69eb56c73f7e8f29bf91c78c4b9411
Srinath V <srinathv35@gmail.com>
2023-06-28 14:58:29 +0530

    committed successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h2> hello welcome to angular</h2>
+  <p> welcome </p>
+  <button type="button" class="btn btn-outline-primary">Srinath</button>
+ 
+
+<h2>Text input fields</h2>
+
+
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="Srinath"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Sree">
+
+
+
+
+ `,
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'Srinath Angular Class ';
